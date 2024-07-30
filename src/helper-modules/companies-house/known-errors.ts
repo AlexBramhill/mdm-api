8f86f208a50dcaa9827028d7ec4f70d1c86af974
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-07-30 15:54:01 +0100

    fix(dtfs2-none): fix handling of change dresponse to companies house endpoint
    companies house no longer sends content on a 404

--- src/helper-modules/companies-house/known-errors.ts
+++ src/helper-modules/companies-house/known-errors.ts
@@ -1,15 +1,39 @@
-import { AxiosError } from 'axios';
+import { AxiosError, isAxiosError } from 'axios';
 
 import { CompaniesHouseInvalidAuthorizationException } from './exception/companies-house-invalid-authorization.exception';
 import { CompaniesHouseMalformedAuthorizationHeaderException } from './exception/companies-house-malformed-authorization-header.exception';
 import { CompaniesHouseNotFoundException } from './exception/companies-house-not-found.exception';
+import { HttpCode, HttpStatus } from '@nestjs/common';
 
 export type KnownErrors = KnownError[];
 
-type KnownError = { caseInsensitiveSubstringToFind: string; throwError: (error: AxiosError) => never };
+type KnownError = { checkHasError: (error: Error) => boolean; throwError: (error: Error) => never };
+
+const checkMessageForSubstring = ({ error, caseInsensitiveSubstring }: { error: Error; caseInsensitiveSubstring: string }): boolean => {
+  if (!isAxiosError(error) || !!error.response || typeof error.response.data === 'object') {
+    return;
+  }
+  const errorResponseData = error.response.data;
+  let errorMessage: string;
+
+  if (typeof errorResponseData.error === 'string') {
+    errorMessage = errorResponseData.error;
+  } else if (errorResponseData.errors && errorResponseData.errors[0] && typeof errorResponseData.errors[0].error === 'string') {
+    errorMessage = errorResponseData.errors[0].error;
+  }
+
+  if (errorMessage) {
+    const errorMessageInLowerCase = errorMessage.toLowerCase();
+    return errorMessageInLowerCase.includes(caseInsensitiveSubstring.toLowerCase());
+  }
+};
+
+const checkForStatus = ({ error, status }: { error: Error; status: HttpStatus }): boolean => {
+  return isAxiosError(error) && error.response?.status === status;
+};
 
 export const getCompanyMalformedAuthorizationHeaderKnownCompaniesHouseError = (): KnownError => ({
-  caseInsensitiveSubstringToFind: 'Invalid Authorization header',
+  checkHasError: (error) => checkMessageForSubstring({ error, caseInsensitiveSubstring: 'Invalid Authorization header' }),
   throwError: (error) => {
     throw new CompaniesHouseMalformedAuthorizationHeaderException(
       `Invalid 'Authorization' header. Check that your 'Authorization' header is well-formed.`,
@@ -19,14 +43,14 @@ export const getCompanyMalformedAuthorizationHeaderKnownCompaniesHouseError = ()
 });
 
 export const getCompanyInvalidAuthorizationKnownCompaniesHouseError = (): KnownError => ({
-  caseInsensitiveSubstringToFind: 'Invalid Authorization',
+  checkHasError: (error) => checkMessageForSubstring({ error, caseInsensitiveSubstring: 'Invalid Authorization' }),
   throwError: (error) => {
     throw new CompaniesHouseInvalidAuthorizationException('Invalid authorization. Check your Companies House API key.', error);
   },
 });
 
 export const getCompanyNotFoundKnownCompaniesHouseError = (registrationNumber: string): KnownError => ({
-  caseInsensitiveSubstringToFind: 'company-profile-not-found',
+  checkHasError: (error) => checkForStatus({ error, status: HttpStatus.NOT_FOUND }),
   throwError: (error) => {
     throw new CompaniesHouseNotFoundException(`Company with registration number ${registrationNumber} was not found.`, error);
   },
--- src/helper-modules/companies-house/wrap-companies-house-http-error-callback.ts
+++ src/helper-modules/companies-house/wrap-companies-house-http-error-callback.ts
@@ -9,26 +9,9 @@ type CompaniesHouseHttpErrorCallback = (error: Error) => ObservableInput<never>;
 export const createWrapCompaniesHouseHttpGetErrorCallback =
   ({ messageForUnknownError, knownErrors }: { messageForUnknownError: string; knownErrors: KnownErrors }): CompaniesHouseHttpErrorCallback =>
   (error: Error) => {
-    if (error instanceof AxiosError && error.response && typeof error.response.data === 'object') {
-      const errorResponseData = error.response.data;
-      let errorMessage: string;
-
-      if (typeof errorResponseData.error === 'string') {
-        errorMessage = errorResponseData.error;
-      } else if (errorResponseData.errors && errorResponseData.errors[0] && typeof errorResponseData.errors[0].error === 'string') {
-        errorMessage = errorResponseData.errors[0].error;
-      }
-
-      if (errorMessage) {
-        const errorMessageInLowerCase = errorMessage.toLowerCase();
-
-        knownErrors.forEach(({ caseInsensitiveSubstringToFind, throwError }) => {
-          if (errorMessageInLowerCase.includes(caseInsensitiveSubstringToFind.toLowerCase())) {
-            return throwError(error);
-          }
-        });
-      }
-    }
+    knownErrors.forEach((knownError) => {
+      knownError.checkHasError(error) && knownError.throwError(error);
+    });
 
     return throwError(() => new CompaniesHouseException(messageForUnknownError, error));
   };
